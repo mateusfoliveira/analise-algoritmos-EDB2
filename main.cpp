ea3a3fd77fbad0f0adf6de98cd495acb1c6bb7b8
Mateus <mateussp2005@gmail.com>
2025-09-19 23:10:56 -0300

    fix(adiciona texto): adiciona as alterações

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 int main (){
 
     std::cout << "--- Bem vindo ao programa de teste empirico de algoritmos!--- \n";
-    std::cout << "Escolha uma das opcoes: \n 1 - Medir busca (vetor ordenado e pior caso)\n" << "2 - Medir ordenacao\n" << "3 - Encerrar execucao\n";
+    std::cout << "O codigo vem com 4 algoritmos, caso queira mudar basta alterar a funcao no codigo e inserir o nome desejado.\n";
+    std::cout << "Caso queira adicionar um algoritmo basta colar nos arquivos correspondentes ou adicionar incluir ele no main.\n";
+    std::cout << "Escolha uma das opcoes: \n1 - Medir busca (vetor ordenado e pior caso)\n" << "2 - Medir ordenacao\n" << "3 - Encerrar execucao\n";
 
     int entrada;
     
